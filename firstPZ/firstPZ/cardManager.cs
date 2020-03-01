7d3028c8c3fcd2f871fe0265b4dadcc1c9137215
Anastasiia Laptii <laptiyanastasia@gmail.com>
2020-03-01 08:50:20 +0200

    deleted useless files

--- firstPZ/firstPZ/cardManager.cs
+++ firstPZ/firstPZ/cardManager.cs
@@ -6,24 +6,16 @@ namespace firstPZ
 {
     class cardManager:cardModel
     {
+        Random random = new Random();
         public cardModel createCard(int id, string name, int luck)
         {
            
             cardModel cardModel = new cardModel();
             cardModel.id = id;
             cardModel.name = name;
-            cardModel.luck = luck;  //rand
+            cardModel.luck = random.Next(10) - luck;
             return cardModel;
         }
-/*        public List<cardModel> createDeck(cardModel cm)
-        {
-            List<cardModel> listCard = new List<cardModel>();
-            for (int i = 0; i < 30; i++)
-            {
 
-            }
-            return listCard;
-
-        }*/
     }
 }
--- firstPZ/firstPZ/playerManager.cs
+++ firstPZ/firstPZ/playerManager.cs
@@ -6,12 +6,13 @@ namespace firstPZ
 {
     class playerManager
     {
+        Random random = new Random();
         public playerModel createPlayer(int id, string name, int cardNum)
         {
             playerModel player = new playerModel();
             player.id = id;
             player.name = name;
-            player.cardNum = 1; /*rand*/
+            player.cardNum = random.Next(5) * cardNum;
             return player;
         }
     }
